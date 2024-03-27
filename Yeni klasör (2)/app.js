7cc1807f44b0d2878eb6836db2e9b10736737b7c
IbrahimovaSara <161681213+IbrahimovaSara@users.noreply.github.com>
2024-03-27 16:22:16 +0300

    Add files via upload

---
+++ Yeni klasör (2)/app.js
@@ -0,0 +1,49 @@
+// let eded=+prompt("eded;");
+// if(eded %2,3,4,5,6,7,8,9==0){
+//     console.log(`${eded}eded murekkeb ededdir`);
+// }else{
+//     console.log(`${eded}eded murekkeb ededdir`);
+// } 
+
+
+
+
+
+// let eded=+prompt("eded:");
+// for(let bolen = 1; bolen <= eded; bolen++ );{
+//     if(eded % bolen == 0)
+// colsone.log(`${eded} ededlerin cemi mukemmel ededdir`)
+// }
+
+
+
+
+// let num1 = +prompt("Number 1: ");
+// let num2 = +prompt("Number 2: ");
+// let ara = num1<let<num2;
+// if (sum % 2 === 0) {  
+//   console.log(`${num1} ve ${num2} ededlerinin arsindaki sade ededletdir ${ara}-dir.`);
+// } 
+
+
+
+
+// let num1 = +prompt("Number 1: ");
+// let num2 = +prompt("Number 2: ");
+// let ara = num1<let<num2;
+// if (sum % 2 === 0) {
+//   console.log(`${num1} ve ${num2} ededlerinin arsindaki mukemmel ededletdir ${ara}-dir.`);
+// }
+
+
+
+
+// let eded=+prompt("eded;");
+
+// if(eded >i && eded>0){
+//     console.log(`${eded}eded mu ededdir`);
+// }else{
+//     console.log(`${eded}eded murekkeb ededdir`);
+// } 
+
+
